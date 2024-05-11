dae3b4af5e16b503c82f03b97a8674435e61b292
tanvir321110 <tanvirahmed321110@gmail.com>
2024-05-12 03:09:00 +0600

    first commist

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+const header = document.querySelector('header');
+const headerNav = header.querySelector('.header-nav');
+const menuBtn = header.querySelector('.mobile-menu');
+const menuCloseBtn = header.querySelector('.close-btn');
+
+
+menuBtn.addEventListener('click',function(){
+    headerNav.classList.add('active')
+})
+
+menuCloseBtn.addEventListener('click',function(){
+    headerNav.classList.remove('active')
+})
+
+
+
+// search button
+const searchBtn = header.querySelector('.searchBtn')
+const searchFrield = document.getElementById('search-frield'); 
+const searchCloseBtn = searchFrield.querySelector('.close-btn')
+
+searchBtn.addEventListener('click',function(){
+    searchFrield.classList.remove('deactive');
+    searchFrield.classList.add('active');
+})
+
+searchCloseBtn.addEventListener('click',function(){
+    searchFrield.classList.remove('active');
+    searchFrield.classList.add('deactive')
+})
